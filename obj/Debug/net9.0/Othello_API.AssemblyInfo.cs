69928910f671a1a9993fd69e1bdcd861c0d1f6f5
AngelaPatarroyo <angela.patarroyo@hotmail.com>
2025-03-01 12:58:13 +0000

    Updated validation and fixed issues

--- obj/Debug/net9.0/Othello_API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Othello_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e583aeadb53bc5ceba68c220f7b460a06e6c4bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1719935d31cebb98f8b53856e21980c1107f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

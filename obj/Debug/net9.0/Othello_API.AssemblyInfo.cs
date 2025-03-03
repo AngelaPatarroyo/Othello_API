667be17eb694959325fe2f871c117a15b2e79f2f
AngelaPatarroyo <angela.patarroyo@hotmail.com>
2025-03-03 12:52:22 +0000

    ignore secrets

--- obj/Debug/net9.0/Othello_API.AssemblyInfo.cs
+++ obj/Debug/net9.0/Othello_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2480dcefa17f69cf2b3f583caebc3aab9bd06728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a258efcf21e317a4f1c86e818ad4c2fc5b6085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Othello_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
